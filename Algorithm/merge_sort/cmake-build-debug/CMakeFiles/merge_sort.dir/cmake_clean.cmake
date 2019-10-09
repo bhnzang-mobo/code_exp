7137bd78890ef2fd76d51b8a0a87c410bc91eb32
MOBO <bhnzang9608@gmail.com>
2019-10-09 18:22:20 +0900

    Merge Sort

---
+++ Algorithm/merge_sort/cmake-build-debug/CMakeFiles/merge_sort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/merge_sort.dir/main.c.obj"
+  "merge_sort.pdb"
+  "merge_sort.exe"
+  "merge_sort.exe.manifest"
+  "libmerge_sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/merge_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
