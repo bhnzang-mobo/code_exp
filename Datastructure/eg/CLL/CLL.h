aa02c5b869fdc55c4a5927bff899cac1c552afd8
MOBO <bhnzang9608@gmail.com>
2020-02-06 23:09:29 +0900

    CLL.h comment modified 20

---
+++ Datastructure/eg/CLL/CLL.h
@@ -0,0 +1,26 @@
+#ifndef __C_Llist_H__
+#define __C_Llist_H__
+
+typedef int Data;
+typedef struct Cnode{
+	Data data;
+	struct Cnode* next;
+}CNode;
+
+typedef struct Clist{
+	CNode* tail;
+	CNode* cur;
+	CNode* before;
+	int count;
+}CList;
+
+//adding new node;
+void Cinit(CList*);
+void Cinsert(CList*,Data);	//insert data on tail
+void CFinsert(CList*,Data);	//insert data on head(front)
+//node reference;
+int CFirst(CList*,CNode*);
+int CNext(CList*,CNode*);
+Data CRemove(CList*);
+int CCount(CList*);
+#endif
---
+++ Datastructure/eg/CLL/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "CLL.h"
+
+int main () {
+	CList cl;
+	Data data;
+	CNode node;
+	Cinit(&cl);
+	
+	Cinsert(&cl,3);
+	Cinsert(&cl,4);
+	Cinsert(&cl,5);
+	CFinsert(&cl,2);
+	CFinsert(&cl,1);
+	
+	int nodnum = CCount(&cl);
+	
+	if(CFirst(&cl,&node)){
+		printf("%d ",node.data);
+		for(int i = 0 ; i < nodnum*3-1; i++){
+			CNext(&cl,&node);
+			printf("%d ",node.data);
+		}
+	}
+	printf("\n");/*
+	if(nodnum!=0){
+		CFirst(&cl,&node);
+		if(node.data%2 == 0){
+			CRemove(&cl);
+		}
+
+		for(int j = 0 ; j < 4; j ++){
+			CNext(&cl,&node);
+			if(node.data%2 == 0){
+				CRemove(&cl);
+			}
+		}
+	}*/
+	if(CFirst(&cl,&node)){
+		printf("%d ",node.data);
+		for(int i = 0 ; i < CCount(&cl)-1; i++){
+			CNext(&cl,&node);
+			printf("%d ",node.data);
+		}
+	}
+	printf("\n");
+	int count = cl.count;
+	CFirst(&cl,&node);
+	for(int i = 0 ; i < count ; i ++){
+		printf("%d ",CRemove(&cl));
+		CNext(&cl,&node); //consequent removal is not approved
+	}
+}
