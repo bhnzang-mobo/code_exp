348578ede8c550d04f6d28fd36712fd09960c9d8
MOBO <bhnzang9608@gmail.com>
2020-02-09 00:36:06 +0900

    infix to postfix converter (bracket included)

---
+++ Datastructure/eg/Stack/calculator/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "stack.h"
+int comp(char a, char b){
+    int ak,bk;
+    if(a=='+'||a=='-'){
+        ak=0;
+    }
+    else{
+        ak=1;
+    }
+    if(b=='+'||b=='-'){
+        bk=0;
+    }
+    else{
+        bk=1;
+    }
+    if(ak<bk){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int main(){
+    Stack stack;
+    Stackinit(&stack);
+    char eq[100];
+    char ret[100];
+    printf("Input equation : ");
+    scanf("%s",eq);
+    int i = 0;
+    int j =0;
+    char ch = eq[i];
+    while(ch!='\0'){
+        if(isdigit(ch)){
+            ret[j++]=ch;
+            ch=eq[++i];
+        }
+        else if(ch=='('){
+            Push(&stack,ch);
+            ch=eq[++i];
+            continue;
+        }
+        else {
+            if (ch == ')') {
+                while (!isEmpty(&stack) && Peek(&stack) != '(') {
+                    ret[j++] = Pop(&stack);
+                }
+                Pop(&stack);
+                ch = eq[++i];
+                continue;
+            }
+            while(!isEmpty(&stack)&&Peek(&stack)!='('&&!comp(Peek(&stack),ch)){
+                ret[j++]=Pop(&stack);
+            }
+            Push(&stack, ch);
+            ch = eq[++i];
+        }
+    }
+    while(!isEmpty(&stack)){
+        ret[j++]=Pop(&stack);
+    }
+    ret[j]='\0';
+    printf("%s",ret);
+    return 0;
+}
